a8e2bbf50ca8e93c0193ce075b2cb740e7757034
BettiM7 <bettinimichelangelo3@gmail.com>
2024-04-14 16:51:53 +0200

    system call, processes

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -37,8 +37,9 @@ process_execute (const char *file_name)
   if (fn_copy == NULL)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
+  char *saveptr;
+  file_name = strtok_r((char*)file_name, " ", &saveptr );
 
-  /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
@@ -86,7 +87,7 @@ start_process (void *file_name_)
    This function will be implemented in problem 2-2.  For now, it
    does nothing. */
 int
-process_wait (tid_t child_tid UNUSED) 
+process_wait (tid_t child_tid)
 {
   return -1;
 }
--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -1,20 +1,94 @@
 #include "userprog/syscall.h"
+#include "userprog/pagedir.h"
+#include "threads/vaddr.h"
 #include <stdio.h>
-#include <syscall-nr.h>
+#include <lib/stdio.h>
+#include <kernel/stdio.h>
+#include <lib/syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+    intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
+bool
+is_valid_ptr(const void *ptr)
 {
-  printf ("system call!\n");
-  thread_exit ();
+    if (ptr == NULL || !is_user_vaddr(ptr) || pagedir_get_page
+                                                      (thread_current()->pagedir, ptr) == NULL) {
+        return false;
+    }
+    return true;
+}
+
+bool has_valid_args(void *ptr, int args){
+    int i;
+    args *= 4; // size of an arg in 32-bit sys
+    for(i = 0; i < args; i++)
+    {
+        if(is_valid_ptr(ptr+i))
+            return false;
+    }
+    return true;
+}
+
+//void exit(int status)
+//{
+//    struct thread *cur = thread_current();
+//
+//    printf("%s: exit(%d)\n", cur->name, status);
+//
+//    /* If its parent is still waiting for it,
+//     tell its parent its exit status. */
+//    if (cur->parent != NULL)
+//    {
+//        cur->parent->child_exit_status = status;
+//        // printf("parent %s: child_exit(%d)\n", cur->parent->name, cur->parent->child_exit_status);
+//    }
+//    thread_exit();
+//}
+
+static void
+syscall_handler (struct intr_frame *f UNUSED) {
+    if (!has_valid_args(f->esp, 1)) {
+        thread_exit();
+        return;
+    }
+    int n_sc = *(int *) f->esp;
+
+    switch (n_sc) {
+        case SYS_EXIT:
+        {
+            if (!has_valid_args(f->esp + 4, 1)) {
+                thread_exit();
+                return;
+            }
+            int status = *(int *) (f->esp + 4);
+            f->eax = status;
+            break;
+        }
+        case SYS_WRITE:
+        {
+            const void *buffer = *(const void **)(f->esp + 8);
+            unsigned size = *(unsigned *)(f->esp + 12);
+
+            if (is_valid_ptr(buffer)) {
+                putbuf(buffer, size);
+                f->eax = size; // Return the number of bytes written.
+            } else {
+                f->eax = -1;
+            }
+            break;
+        }
+        default:
+        {
+            printf("Other sys_call: %d\n", n_sc);
+            break;
+        }
+    }
 }
